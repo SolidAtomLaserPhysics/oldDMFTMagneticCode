b99b93eb2165ccda4875bc5c9ac5968ec5ebc99d
Nico Hyttrek <nhyttrek@th1-2021-d03.PHYSnet.uni-hamburg.de>
2023-03-14 14:09:05 +0100

    Better Sampling

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 
 
 cp hubb.andpar_bak hubb.andpar
-mpif90 ed_dmft_parallel_frequencies_corrected_and_commented.f -llapack -o run.x  -ffixed-line-length-0  #-fcheck=all
+mpif90 ed_dmft_parallel_frequencies_corrected_and_commented_betterSampling.f -llapack -o run.x  -ffixed-line-length-0  #-fcheck=all
 mpirun -np 36 --oversubscribe ./run.x > run.out
